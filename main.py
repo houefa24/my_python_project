b75da882c50ee035628a896bb2e407c89b9ede30
Romance DAH <h.dahhoundogbanon@ecoles-epsi.net>
2025-05-14 01:50:56 +0200

    all

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ def hello(name="GitHub Actions", lastname=None):
     if not isinstance(name, str):
         raise TypeError("Le nom doit être une chaîne")
 
+
     # Supprime ou commente cette ligne après le test de gestion d'erreur
-    raise ValueError("Erreur volontaire")  
+    #raise ValueError("Erreur volontaire")
+    
+    
     if lastname:
         return f"Hello, {name} {lastname} !"
     
-    return f"Hello, {name} !"  # Assure-toi d'avoir l'espace avant le point d'exclamation
+    return f"Hello, {name} !"
