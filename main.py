753e8cbc3f963d58305aaa7fd23b246f5dc22820
Romance DAH <h.dahhoundogbanon@ecoles-epsi.net>
2025-05-14 01:55:55 +0200

    .

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def hello(name="GitHub Actions", lastname=None):
     if not isinstance(name, str):
         raise TypeError("Le nom doit être une chaîne")
-    
+
     if lastname:
         return f"Hello, {name} {lastname} !"
-    
+
     return f"Hello, {name} !"
